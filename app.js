cd9179a3a0148fe359c45fe5e40ce313ce1ecfe4
Muzaffar Yusupov <themuzaffar7@gmail.com>
2024-01-28 22:43:55 +0500

    first -commit

---
+++ app.js
@@ -0,0 +1,25 @@
+
+let color_2 = document.querySelector('.header__btn__title');
+
+color_2.addEventListener('click',function(){
+    color_2.style.color = 'red';
+})
+
+let color = document.querySelector('.header__btn');
+
+color.addEventListener('click',function(){
+    color.style.backgroundColor = 'yellow';
+})
+
+let btn = document.querySelector('.header__content__btn');
+
+btn.addEventListener('click',function(){
+    btn.style.backgroundColor = 'yellow';
+})
+
+
+let btn_two = document.querySelector('.header__content__btn_two');
+
+btn_two.addEventListener('click',function(){
+    btn_two.style.backgroundColor = 'red';
+})
