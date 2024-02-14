c3a74d38fe3a9e1ba998241f31cacda4f3540687
Muzaffar Yusupov <themuzaffar7@gmail.com>
2024-02-14 13:57:20 +0500

    change site

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
+const headerButton = document.querySelector('.header__btn')
 
-let color_2 = document.querySelector('.header__btn__title');
-
-color_2.addEventListener('click',function(){
-    color_2.style.color = 'red';
-})
-
-let color = document.querySelector('.header__btn');
-
-color.addEventListener('click',function(){
-    color.style.backgroundColor = 'yellow';
-})
-
-let btn = document.querySelector('.header__content__btn');
-
-btn.addEventListener('click',function(){
-    btn.style.backgroundColor = 'yellow';
-})
-
-
-let btn_two = document.querySelector('.header__content__btn_two');
-
-btn_two.addEventListener('click',function(){
-    btn_two.style.backgroundColor = 'red';
+headerButton.addEventListener('click', function(){
+    headerButton.classList.add('active')
 })
